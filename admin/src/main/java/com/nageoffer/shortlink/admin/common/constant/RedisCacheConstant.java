3db9834786aeca909cc01bb0989db1bb42df29cf
itcast <hello@itcast.cn>
2023-12-09 15:58:55 +0800

    feature:通过分布式锁完善用户注册功能,防止恶意请求短时间内使用同一未注册用户大量注册新用户

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,5 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/Impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/Impl/UserServiceImpl.java
@@ -12,10 +12,13 @@ import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDto;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
 import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
@@ -29,17 +32,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Autowired
     private RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
-        if (userDO==null){
+        if (userDO == null) {
             throw new ClientException(UserErrorCodeEnum.USER_NULL);
         }
         UserRespDTO userRespDTO = new UserRespDTO();
-        BeanUtils.copyProperties(userDO,userRespDTO);
+        BeanUtils.copyProperties(userDO, userRespDTO);
 
         return userRespDTO;
     }
@@ -48,28 +53,46 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
      * hasUsername方法接收一个字符串类型的参数username，用于检查用户名是否已经被注册过。
      * 它直接调用userRegisterCachePenetrationBloomFilter的contains方法，
      * 传入username作为参数，返回布尔值表示用户名是否存在于布隆过滤器中。
+     *
      * @param username
      * @return
      */
 
     @Override
     public Boolean hasUsername(String username) {
-      return !userRegisterCachePenetrationBloomFilter.contains(username);
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
     }
 
+
+    /**
+     * 注册功能
+     * 使用redisson分布式锁 防止恶意请求毫秒级触发大量请求去一个未注册的用户名
+     * @param requestParam
+     */
     @Override
     public void register(UserRegisterReqDto requestParam) {
         if (!hasUsername(requestParam.getUsername())) {
             //当前用户已经存在
             throw new ClientException(USER_NAME_EXIST);
         }
-        //用户名不存在 将用户名存入数据库
-        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (insert<1){
-            throw new ClientException(USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                //用户名不存在 将用户名存入数据库
+                int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (insert < 1) {
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+
+        } finally {
+            lock.unlock();
         }
 
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
     }
 
 
