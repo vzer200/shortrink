c0a16434f84938eea59e28a52059ba35fe749c0b
itcast <hello@itcast.cn>
2024-02-12 13:47:11 +0800

    refactor:重构短链接系统引入网关架构

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -2,18 +2,18 @@ package com.nageoffer.shortlink.admin.common.enums;
 
 import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
 
+/**
+ * 用户错误码
+ */
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    //逗号结尾
-    USER_TOKEN_FAIL("A000200","用户Token验证失败"),
     USER_NULL("B000200", "用户记录不存在"),
-    USER_NAME_EXIST("B000201","用户名已存在"),
+
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+
     USER_EXIST("B000202", "用户记录已存在"),
+
     USER_SAVE_ERROR("B000203", "用户记录新增失败");
-
-
-
-    // ... 其他枚举值
 
     private final String code;
 
@@ -34,3 +34,4 @@ public enum UserErrorCodeEnum implements IErrorCode {
         return message;
     }
 }
+
